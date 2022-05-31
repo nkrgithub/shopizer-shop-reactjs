86d7ff38b3d3feb6e9c6bda7ac7fa060a9f1d288
ujjwal-shrivastava <ujjwal.srivastava@primewayz.com>
2022-05-31 12:25:15 +0530

    Adding the old Home.js with zero changes on first commit branch

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,8 +3,7 @@ import MetaTags from "react-meta-tags";
 import Layout from "../../layouts/Layout";
 import HeroSlider from "../../wrappers/hero-slider/HeroSlider";
 import TabProduct from "../../wrappers/product/TabProduct";
-import NewProduct from "../../wrappers/product/NewProduct";
-import FeatureIcon from "../../wrappers/feature-icon/FeatureIcon";
+// import FeatureIcon from "../../wrappers/feature-icon/FeatureIcon";
 import Promo from "../../wrappers/promos/Promos";
 import Newsletter from "../../wrappers/newsletter/Newsletter";
 import { multilanguage } from "redux-multilanguage";
@@ -34,22 +33,14 @@ const Home = ({ merchant, strings }) => {
           spaceTopClass="pt-50"
           spaceBottomClass="pb-40"
         />
-
+        
         {/* tab product */}
         <TabProduct
           category="fashion"
           spaceBottomClass="pb-100"
           spaceTopClass="pt-100"
         />
-
-        {/* new product */}
-        {/* <NewProduct
-          category="fashion"
-          spaceBottomClass="pb-100"
-          spaceTopClass="pt-100"
-        /> */}
-
-        {/* feature icon */}
+        {/* feature icon 
         <FeatureIcon
           bgImg="/assets/img/bg/shape.png"
           containerClass="container-fluid"
@@ -57,7 +48,7 @@ const Home = ({ merchant, strings }) => {
           spaceTopClass="pt-50"
           spaceBottomClass="pb-40"
         />
-
+        */}
         {/* newsletter */}
         <Newsletter
           spaceTopClass="pt-100"
@@ -69,11 +60,13 @@ const Home = ({ merchant, strings }) => {
   );
 };
 
-const mapStateToProps = (state) => {
+
+const mapStateToProps = state => {
   return {
-    merchant: state.merchantData.merchant,
+    merchant: state.merchantData.merchant
   };
 };
+
 
 export default connect(mapStateToProps, null)(multilanguage(Home));
 // export default HomeFashionSeven;
